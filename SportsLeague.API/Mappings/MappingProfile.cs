28e84e99f688d7d99aee69e00b0468219b814fd7
Jeronimo <castrillonj041@gmail.com>
2026-03-25 18:32:52 -0500

    Fase 2 terminada

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -2,9 +2,9 @@
 using SportsLeague.API.DTOs.Request;
 using SportsLeague.API.DTOs.Response;
 using SportsLeague.Domain.Entities;
-
+ 
 namespace SportsLeague.API.Mappings;
-
+ 
 public class MappingProfile : Profile
 {
     public MappingProfile()
@@ -12,7 +12,7 @@ public class MappingProfile : Profile
         // Team mappings
         CreateMap<TeamRequestDTO, Team>();
         CreateMap<Team, TeamResponseDTO>();
-
+ 
         // Player mappings
         CreateMap<PlayerRequestDTO, Player>();
         CreateMap<Player, PlayerResponseDTO>()
