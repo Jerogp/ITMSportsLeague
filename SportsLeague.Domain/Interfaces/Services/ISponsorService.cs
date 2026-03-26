7e968d7d505c14d3d9126a92d4d768f16de996cd
Jeronimo <castrillonj041@gmail.com>
2026-03-26 13:11:03 -0500

    Update the DbContext Interfaces created: ISponsorRepository, ITournamentSponsorRepository and ISponsorService

---
+++ SportsLeague.Domain/Interfaces/Services/ISponsorService.cs
@@ -0,0 +1,18 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services;
+
+public interface ISponsorService
+{
+    // CRUD Sponsor
+    Task<IEnumerable<Sponsor>> GetAllAsync();
+    Task<Sponsor?> GetByIdAsync(int id);
+    Task<Sponsor> CreateAsync(Sponsor sponsor);
+    Task UpdateAsync(int id, Sponsor sponsor);
+    Task DeleteAsync(int id);
+
+    // Lógica N:M
+    Task<IEnumerable<TournamentSponsor>> GetTournamentsBySponsorAsync(int sponsorId);
+    Task<TournamentSponsor> LinkToTournamentAsync(int sponsorId, int tournamentId, decimal contractAmount);
+    Task UnlinkFromTournamentAsync(int sponsorId, int tournamentId);
+}
